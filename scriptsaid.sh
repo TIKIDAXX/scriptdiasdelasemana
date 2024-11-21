9c96d4e737a1f604cf708ddcb305642c029f79d2
TIKIDAXX <hap.mestre77@gmail.com>
2024-11-21 15:34:13 +0100

    examen 21 de noviembre

---
+++ scriptsaid.sh
@@ -0,0 +1,27 @@
+#!/usr/env/bin bash
+mkdir  2025
+cd ./2025
+mkdir enero
+cd ./enero
+mkdir semana1
+cd ./semana1
+mkdir miercoles jueves viernes sabado domingo lunes martes
+cd ..
+mkdir semana2
+cd ./semana2
+mkdir miercoles jueves viernes sabado domingo lunes martes
+cd ..
+mkdir semana3
+cd ./semana3
+mkdir miercoles jueves viernes sabado domingo lunes martes
+cd ..
+mkdir semana4
+cd ./semana4
+mkdir miercoles jueves viernes sabado domingo lunes martes
+cd ..
+mkdir semana5
+cd ./semana5
+mkdir miercoles jueves viernes 
+cd ..
+
+
